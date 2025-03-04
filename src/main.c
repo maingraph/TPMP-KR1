f9cd2c129c5e5ea5d964b64d821bc7b55fc3e9d7
imjustchilling <andreyalekswork@gmail.com>
2025-03-04 09:21:30 +0300

    Initial + added readme

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Control test 1, 11a, Buinevich Andrey Aleksandrovich\n");
+    printf("Контрольная работа 1, 11а, Буйневич Андрей Александрович");
+
+}
